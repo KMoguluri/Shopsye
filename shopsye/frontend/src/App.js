b93deeebd502ec1275dbe0a91637cf42da305343
Stack_Pointer <kiranchamp.6457@gmail.com>
2023-02-23 20:34:19 -0600

    first commit

---
+++ shopsye/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="https://www.amazon.com/">Shopsye</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
