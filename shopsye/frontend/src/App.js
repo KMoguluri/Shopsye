df28e3944197a9743c606c62314c6d2c8a7cb4ce
Stack_Pointer <kiranchamp.6457@gmail.com>
2023-02-23 22:12:20 -0600

    test user

--- shopsye/frontend/src/App.js
+++ shopsye/frontend/src/App.js
@@ -2,7 +2,11 @@ function App() {
   return (
     <div>
       <header>
+<<<<<<< Updated upstream:shopsye/frontend/src/App.js
         <a href="/">Shopsye kiran Kumar</a>
+=======
+        <a href="/">Amazona</a>
+>>>>>>> Stashed changes:src/App.js
       </header>
       <main>List of products</main>
     </div>
